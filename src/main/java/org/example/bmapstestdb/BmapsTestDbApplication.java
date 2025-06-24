1c1daa1174da0987266b3d73909df8d12eae6ec0
Kanen1323 <safari.rre.345@gmail.com>
2025-06-24 12:44:05 +0300

    first commit

---
+++ src/main/java/org/example/bmapstestdb/BmapsTestDbApplication.java
@@ -0,0 +1,13 @@
+package org.example.bmapstestdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmapsTestDbApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BmapsTestDbApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/bmapstestdb/BmapsTestDbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bmapstestdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmapsTestDbApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
